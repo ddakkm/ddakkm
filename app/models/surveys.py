585ad5932e91a6558f80e527cfadcba5015a391c
sean_macbook <ddhyun93@gmail.com>
2021-09-30 15:23:00 +0900

    EDIT Enum type of SQLAlchemy models

--- app/models/surveys.py
+++ app/models/surveys.py
@@ -1,21 +1,37 @@
-from sqlalchemy import Column, Integer, Boolean, ForeignKey
+import enum
+
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, Enum
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.mysql import ENUM, JSON
+from sqlalchemy.dialects.mysql import JSON
 
 from app.db.base_class import Base
 
-VACCINE_TYPE = ("PFIZER", "MODERNA", "AZ", "JANSSEN", "ETC")
-DATE_FROM = ("0", "2", "3", "over5", "over1week", "over1month")
+
+class VaccineType(str, enum.Enum):
+    PFIZER = "PFIZER"
+    MODERNA = "MODERNA"
+    AZ = "AZ"
+    JANSSEN = "JANSSEN"
+    ETC = "ETC"
+
+
+class DATE_FROM(str, enum.Enum):
+     ZERO = "ZERO"
+     TWO_DAYS = "TWO_DAYS"
+     THREE_DAYS = "THREE_DAYS"
+     OVER_FIVE = "OVER_FIVE"
+     OVER_WEEK = "OVER_WEEK"
+     OVER_MONTH = "OVER_MONTH"
 
 
 class SurveyA(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    vaccine_type = Column(ENUM(*VACCINE_TYPE), default=VACCINE_TYPE[0])
+    vaccine_type = Column(Enum(VaccineType), default=VaccineType.ETC)
     is_crossed = Column(Boolean, default=False)
     is_pregnant = Column(Boolean, default=False)
     is_underlying_disease = Column(Boolean, default=False)
-    date_from = Column(ENUM(*DATE_FROM), default=DATE_FROM[0])
+    date_from = Column(Enum(DATE_FROM), default=DATE_FROM.ZERO)
     data = Column(JSON)
 
     # One to One
--- app/models/users.py
+++ app/models/users.py
@@ -1,25 +1,45 @@
-from sqlalchemy import Column, Integer, String, Boolean
+import enum
+
+from sqlalchemy import Column, Integer, String, Boolean, Enum
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.mysql import ENUM
+# from sqlalchemy.dialects.mysql import ENUM
 
 
 from app.db.base_class import Base
 
-SNS_PROVIDER = ('LOCAL', 'NAVER', 'KAKAO')
-GENDER = ('ETC', 'MALE', 'FEMALE')
-JOIN_SURVEY_CODE = ('None', 'A', 'B', 'C')
+
+class SnsProviderType(str, enum.Enum):
+    LOCAL = "LOCAL"
+    NAVER = "NAVER"
+    KAKAO = "KAKAO"
+
+
+class Gender(str, enum.Enum):
+    ETC = "ETC"
+    MALE = "MALE"
+    FEMALE = "FEMALE"
+
+
+class JoinSurveyCode(str, enum.Enum):
+    NONE = "NONE"
+    A = "A"
+    B = "B"
+    C = "C"
 
 
 class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     is_super = Column(Boolean, default=False)
     is_active = Column(Boolean, default=True)
-    sns_provider = Column(ENUM(*SNS_PROVIDER), default=SNS_PROVIDER[0])
+    # sns_provider = Column(ENUM(*SNS_PROVIDER), default=SNS_PROVIDER[0])
+    sns_provider = Column(Enum(SnsProviderType), default=SnsProviderType.LOCAL)
     email = Column(String(100), unique=True, index=True, nullable=False)
     nickname = Column(String(30), unique=True, nullable=False)
-    gender = Column(ENUM(*GENDER), default=GENDER[0])
+    # gender = Column(ENUM(*GENDER), default=GENDER[0])
+    gender = Column(Enum(Gender), default=Gender.ETC)
     age = Column(Integer, default=1980)
-    join_survey_code = Column(ENUM(*JOIN_SURVEY_CODE), default=JOIN_SURVEY_CODE[0])
+    # join_survey_code = Column(ENUM(*JOIN_SURVEY_CODE), default=JOIN_SURVEY_CODE[0])
+    join_survey_code = Column(Enum(JoinSurveyCode), default=JoinSurveyCode.NONE)
 
     # One to One
     survey_a = relationship("SurveyA", back_populates="user", uselist=False)
